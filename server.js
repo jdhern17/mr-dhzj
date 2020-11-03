865b676aa896c3f6ae31ba282df90b6c25a104d1
jdhgit <17481018+jdhern17@users.noreply.github.com>
2020-11-02 17:21:37 -0700

    added frontend homepage

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 // define behavior for serving assets in production
 if (process.env.NODE_ENV === "production") {
-  console.log("******__dirname is...*******", __dirname);
-  console.log("******__dirname/client/public resolution is...*******", path.resolve(__dirname, "client/public"));
-  console.log("******__dirname/client/public joined is...*******", path.join(__dirname, "client/public"));
+  // console.log("******__dirname is...*******", __dirname);
+  // console.log("******__dirname/client/public resolution is...*******", path.resolve(__dirname, "client/public"));
+  // console.log("******__dirname/client/public joined is...*******", path.join(__dirname, "client/public"));
   app.use(express.static("client/public"));
   // app.use("/static", express.static(path.join(__dirname, "client/public")));
 }
