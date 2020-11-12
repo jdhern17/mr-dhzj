6b3b6863ae5f7dec0eabc9ee4b741b25c3e10d48
jdhgit <17481018+jdhern17@users.noreply.github.com>
2020-11-12 15:03:54 -0700

    created card for email page

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import mail from '../../assets/images/mail_logo.png'
 
 const Contact = () => {
     return ( <>
@@ -9,7 +10,18 @@ const Contact = () => {
       </div>
       <div className="row">
         <div className="col-lg-10 col-sm-12 col-md-12">
-          Email: hernandez.d.john@gmail.com
+        <div class="card text-center" style={{width: "18rem"}}>
+            <div class="card-header">My Email</div>
+            <img src={mail} class="card-img-top" alt="Resume" />
+            <div class="card-body">
+              {/* <h5 class="card-title">Card title</h5> */}
+              <p class="card-text">
+hernandez.d.john@gmail.com              </p>
+              <a href="mailto:hernandez.d.john@gmail.com" class="btn btn-primary" target="_blank" rel="noopener noreferrer">
+                Send me an Email!
+              </a>
+            </div>
+          </div>
 </div>
 </div>
 
